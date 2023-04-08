b2765435a41a232dddaef4024efbd8a456e65fea
kohei.yamamoto <kohei.yamamoto1012@gmail.com>
2023-04-08 16:50:54 +0900

    testディレクトリ下の全てのテストを実行するように変更

---
+++ lib/rgb.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+def to_hex(red, green, blue)
+  [red, green, blue].sum('#') do |n|
+    n.to_s(16).rjust(2, '0')
+  end
+end
+
+def to_ints(hex)
+  hex.scan(/\w\w/).map(&:hex)
+end
